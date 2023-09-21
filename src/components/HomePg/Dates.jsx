410247988eba9e84522576014857f2695d9356ba
Ayatelrahman Elsayed <aya.n.elsayed@gmail.com>
2023-09-21 17:43:31 +0300

    making dates selection dynamic

---
+++ src/components/HomePg/Dates.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+
+export const Dates = () => {
+
+
+  const [month, setmonth] = useState(8);
+  const [year, setyear] = useState(2023);
+
+  function handlePre() {
+    if (month === 1) {
+      setmonth(12);
+      setyear(year - 1);
+    }
+
+    else {
+      setmonth(month - 1);
+    }
+
+  }
+
+
+  function handlePost() {
+    setmonth(month === 12 ? 1 : month + 1)
+    if (month === 12) {
+      setmonth(1);
+      setyear(year + 1);
+    }
+
+    else {
+      setmonth(month + 1);
+    }
+  }
+
+
+  const monthArr = [
+    '', 'يناير', 'فبراير', 'مارس', 'إبريل', 'مايو', 'يونيو',
+    'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'
+  ];
+
+
+  return (
+    <div className="datesControler d-flex  justify-content-evenly align-items-center">
+      <div onClick={handlePre} className="prev">
+        <button
+          type="button"
+          className="  d-flex   align-items-center "
+        >
+          <img
+            className="btnicons "
+            alt="Image"
+            src="./images/arrow-right.svg"
+          />
+          <p className="month">{month === 1 ? monthArr[12] : monthArr[month - 1]}</p>
+          <p className="date"><span>{month === 1 ? 12 : month - 1}</span>/{month === 1 ? year - 1 : year}</p>
+        </button>
+      </div>
+
+      <div className="currentMonth dropdown ">
+        <button class=" dropdown-toggle " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+         <span className="ms-2"> {monthArr[month]} {month}/{year}</span>
+        </button>
+        <ul class="dropdown-menu me-3 overflow-scroll text-center " style={{ height: '200%' }} aria-labelledby="dropdownMenuButton1">
+
+          {monthArr.map((mnth, indx)=>(
+            <li class="dropdown-item" onClick={() => { setmonth(indx) }}>{mnth}</li>
+          ))}
+          {/* <li><a class="dropdown-item" href="#">{month+1}</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li> */}
+        </ul>
+      </div>
+
+      <div onClick={handlePost} className="next">
+        <button
+          type="button"
+          className="  d-flex align-items-center "
+        >
+
+          <p className="month"> {month === 12 ? monthArr[1] : monthArr[month + 1]} </p>
+          <p className="date"><span>{month === 12 ? 1 : month + 1}</span>/ {month === 12 ? year + 1 : year}</p>
+          <img
+            className="btnicons "
+            alt="Image"
+            src="./images/arrow-left.svg"
+          />
+
+        </button>
+      </div>
+    </div>
+  );
+}
