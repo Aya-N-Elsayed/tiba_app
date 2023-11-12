31b09292d4dece913f03df60ab571afe3feac609
Ayatelrahman Elsayed <aya.n.elsayed@gmail.com>
2023-11-12 17:06:19 +0200

    style

--- src/components/Btns/NewPatientBtn.jsx
+++ src/components/Btns/NewPatientBtn.jsx
@@ -6,10 +6,11 @@ export const NewPatientBtn = ({setShowPopup, showPopup}) => {
     return (
         <button
             type="button"
-            className={styles.newPatientBtn}
+            className={`${styles.newPatientBtn} d-flex`}
             onClick={() => setShowPopup({...showPopup, option: 'p'})}
         >
-            <h6 className='m-0 p-0'> اضافة مريض </h6>
+            <img src="/images/circleBlue.svg" alt="add"  className='ms-2'/>
+            <h6 className='m-0 p-0 fs-5'> اضافة مريض </h6>
         </button>
     );
 }
